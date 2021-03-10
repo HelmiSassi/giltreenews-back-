ea9f126a128be954bfdce32960f647faf0939f6c
HelmiSassi <c.helmi@live.fr>
2021-03-10 02:43:35 +0100

    [HS] mise a jour index.js : port serveur

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use('/user', user);
 app.use('/comments', comments);
 app.use('/preferences', preferences);
 
-var server = {
-    port: 8080
-  };
-  
-  app.listen(server.port, () => console.log(`Server started, listening port: ${server.port}`));
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
